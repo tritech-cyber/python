afb428092703e9b86755824b751d92a32bc3c88e
cwc <icebowl1967@gmail.com>
2021-02-24 19:33:29 -0800

    AA

--- picoCTF/rot13list.py
+++ picoCTF/rot13list.py
@@ -2,21 +2,19 @@ estr = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
 astr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 def main():
-   print(estr)
-   print(astr)
-   a = "c"
-   b = "cvpbPGS{abg_gbb_onq_bs_n_ceboyrz}"
-   n =  estr.find(a)
-   print(astr[n])
-	
+	print(estr)
+	print(astr)
+	a = "c"
+	b = "cvpbPGS{abg_gbb_onq_bs_n_ceboyrz}"
+	for i range (0,len(b)):
+		n =  estr.find(i)
+		print(astr[n])
+
 if __name__ == "__main__":
    main()
 
-
 '''
 txt = "Hello, welcome to my world."
-
 x = txt.find("welcome")
-
 print(x) 
 '''
