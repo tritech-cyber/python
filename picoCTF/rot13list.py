5eb102c92e4c0b15f2170ff75539f37053faedaf
cwc <icebowl1967@gmail.com>
2021-02-24 20:06:57 -0800

    AA

--- picoCTF/rot13list.py
+++ picoCTF/rot13list.py
@@ -1,15 +1,16 @@
-estr = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
-astr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+estr = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm{}_"
+astr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz{} "
 
 def main():
 	print(estr)
 	print(astr)
 	a = "c"
 	b = "cvpbPGS{abg_gbb_onq_bs_n_ceboyrz}"
-	for i range (0,len(b)):
-		n =  estr.find(i)
-		print(astr[n])
-
+	for i in range (0,len(b)):
+		c = b[i]
+		n =  estr.find(c)
+		print(astr[n],end="")
+	print()
 if __name__ == "__main__":
    main()
 
